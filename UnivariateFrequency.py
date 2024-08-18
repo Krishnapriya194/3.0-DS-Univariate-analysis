29bbca5b464bb966861ab448d21c58bc06ac93aa
Krishna Priya S <151844602+Krishnapriya194@users.noreply.github.com>
2024-08-18 21:38:52 +0530

    Add files via upload

--- UnivariateFrequency.py
+++ UnivariateFrequency.py
@@ -12,19 +12,19 @@ class UnivariatePlacement():
         import pandas as pd
         descriptive=pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","Q4:100%","IQR","1.5rule","Lesser","Greater","Min","Max"],columns=quan)
         for columnName in quan:
-            descriptive[columnName]["Mean"]=dataset[columnName].mean()
-            descriptive[columnName]["Median"]=dataset[columnName].median()
-            descriptive[columnName]["Mode"]=dataset[columnName].mode()[0]
-            descriptive[columnName]["Q1:25%"]=dataset.describe()[columnName]["25%"]
-            descriptive[columnName]["Q2:50%"]=dataset.describe()[columnName]["50%"]
-            descriptive[columnName]["Q3:75%"]=dataset.describe()[columnName]["75%"]
-            descriptive[columnName]["Q4:100%"]=dataset.describe()[columnName]["max"]
-            descriptive[columnName]["IQR"]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
-            descriptive[columnName]["1.5rule"]=1.5*descriptive[columnName]["IQR"]
-            descriptive[columnName]["Lesser"]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5rule"]
-            descriptive[columnName]["Greater"]=descriptive[columnName]["Q3:75%"]+descriptive[columnName]["1.5rule"]
-            descriptive[columnName]["Min"]=dataset[columnName].min()
-            descriptive[columnName]["Max"]=dataset[columnName].max()             
+            descriptive.loc["Mean",columnName]=dataset[columnName].mean()
+            descriptive.loc["Median",columnName]=dataset[columnName].median()
+            descriptive.loc["Mode",columnName]=dataset[columnName].mode()[0]
+            descriptive.loc["Q1:25%",columnName]=dataset.describe()[columnName]["25%"]
+            descriptive.loc["Q2:50%",columnName]=dataset.describe()[columnName]["50%"]
+            descriptive.loc["Q3:75%",columnName]=dataset.describe()[columnName]["75%"]
+            descriptive.loc["Q4:100%",columnName]=dataset.describe()[columnName]["max"]
+            descriptive.loc["IQR",columnName]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
+            descriptive.loc["1.5rule",columnName]=1.5*descriptive[columnName]["IQR"]
+            descriptive.loc["Lesser",columnName]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5rule"]
+            descriptive.loc["Greater",columnName]=descriptive[columnName]["Q3:75%"]+descriptive[columnName]["1.5rule"]
+            descriptive.loc["Min",columnName]=dataset[columnName].min()
+            descriptive.loc["Max",columnName]=dataset[columnName].max()             
         return descriptive
     def OutlierColumn(quan,descriptive):    
         lesser=[]
@@ -38,9 +38,9 @@ class UnivariatePlacement():
 
     def replacing_outliers(descriptive,dataset,lesser,greater):
         for columnName in lesser:
-            dataset[columnName][dataset[columnName]<descriptive[columnName]["Lesser"]]=descriptive[columnName]["Lesser"]
+            dataset.loc[dataset[columnName]<descriptive.loc["Lesser",columnName]]=descriptive.loc["Lesser",columnName]
         for columnName in greater:
-            dataset[columnName][dataset[columnName]>descriptive[columnName]["Greater"]]=descriptive[columnName]["Greater"]
+            dataset.loc[dataset[columnName]>descriptive.loc["Greater",columnName]]=descriptive.loc["Greater",columnName]
         return descriptive
 
     def freqTable(quan,dataset):
